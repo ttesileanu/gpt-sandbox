1794b5736fd47047ee69245898500b23aa463cb8
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2023-05-31 13:54:51 -0400

    Add multi-head attention

--- sandbox/first-gpt.py
+++ sandbox/first-gpt.py
@@ -241,4 +241,222 @@ with dv.FigureManager() as (_, ax):
 # %%
 print(untokenize(bigram.generate(context=torch.tensor([[0]]), n=500)[0].tolist()))
 
+
+# %% [markdown]
+# ## Transformer model
+
+
+class SelfAttentionHead(nn.Module):
+    def __init__(self, embedding_size: int, head_size: int, block_size: int):
+        super().__init__()
+
+        self.embedding_size = embedding_size
+        self.head_size = head_size
+        self.block_size = block_size
+
+        self.key = nn.Linear(embedding_size, head_size, bias=False)
+        self.query = nn.Linear(embedding_size, head_size, bias=False)
+        self.value = nn.Linear(embedding_size, head_size, bias=False)
+        self.register_buffer(
+            "mask", torch.tril(torch.ones(block_size, block_size)) == 0
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        T = k.shape[1]
+        w = q @ k.transpose(-1, -2) * self.head_size ** (-0.5)
+        w.masked_fill_(self.mask[:T, :T], float("-inf"))
+        w = F.softmax(w, dim=-1)
+
+        out = w @ v
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self, n_heads: int, embedding_size: int, head_size: int, block_size: int
+    ):
+        super().__init__()
+
+        self.embedding_size = embedding_size
+        self.head_size = head_size
+        self.block_size = block_size
+
+        self.heads = nn.ModuleList(
+            [
+                SelfAttentionHead(embedding_size, head_size, block_size)
+                for _ in range(n_heads)
+            ]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+class TransformerLanguageModel(nn.Module):
+    def __init__(
+        self,
+        vocabulary_size: int,
+        embedding_size: int,
+        head_size: int,
+        block_size: int,
+        n_heads: int = 4,
+    ):
+        super().__init__()
+
+        self.vocabulary_size = vocabulary_size
+        self.embedding_size = embedding_size
+        self.head_size = head_size
+        self.block_size = block_size
+
+        # embed integer tokens as discrete points in continuous space
+        self.token_embedding_table = nn.Embedding(vocabulary_size, embedding_size)
+        # embed positions in the same continuous space
+        self.position_embedding_table = nn.Embedding(block_size, embedding_size)
+
+        assert head_size % n_heads == 0
+        self.sa_heads = MultiHeadAttention(
+            n_heads, embedding_size, head_size // n_heads, block_size
+        )
+        # the language model head converts from embedding space to logits on tokens
+        self.lm_head = nn.Linear(head_size, vocabulary_size)
+
+    def forward(
+        self,
+        context: torch.Tensor,
+        targets: Optional[torch.Tensor] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Forward pass.
+
+        :param context: shape `(batch_size, time_steps)`
+        :param targets: shape `(batch_size, time_steps)`
+        :return: shape `(batch_size, time_steps, vocabulary_size)`
+        """
+        embedded_context = self.token_embedding_table(context)
+        positions = torch.arange(context.shape[1], device=context.device)
+        embedded_position = self.position_embedding_table(positions)
+
+        x = embedded_context + embedded_position
+        x = self.sa_heads(x)
+        logits = self.lm_head(x)
+
+        if targets is not None:
+            B, T, C = logits.shape
+            logits_view = logits.view(B * T, C)
+            targets_view = targets.view(B * T)
+            loss = F.cross_entropy(logits_view, targets_view)
+        else:
+            loss = None
+
+        return logits, loss
+
+    def generate(self, n: int, context: torch.Tensor) -> torch.Tensor:
+        """Generate tokens starting with context.
+
+        :param n: number of tokens to generate
+        :param context: shape `(batch_size, time_steps)`
+        :return: shape `(batch_size, time_steps + n)`
+        """
+        for i in range(n):
+            context_crop = context[:, -self.block_size :]
+            logits, loss = self(context_crop)
+
+            # focus only on last time step
+            logits = logits[:, -1, :]  # shape (B, C)
+            probs = F.softmax(logits, dim=-1)
+
+            # sample
+            next_tokens = torch.multinomial(probs, num_samples=1)  # shape (B, 1)
+            context = torch.cat((context, next_tokens), dim=1)  # shape (B, T + 1)
+
+        return context
+
+
+# %% [markdown]
+# ## Train and try the transformer model
+torch.manual_seed(42)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+embedding_size = 32
+head_size = 32
+transformer = TransformerLanguageModel(
+    len(vocabulary),
+    embedding_size=embedding_size,
+    head_size=head_size,
+    block_size=block_size,
+).to(device)
+optimizer = torch.optim.AdamW(transformer.parameters(), lr=1e-3)
+
+block_size = 8
+batch_size = 32
+n_batches = 15_000
+n_test_batches = 200
+eval_history = []
+loss_history = defaultdict(list)
+pbar = tqdm(range(n_batches))
+
+t0 = time.time()
+eval_interval = 0.5
+for i in pbar:
+    xb, yb = get_batch(train_data, batch_size=batch_size, block_size=block_size)
+
+    xb = xb.to(device)
+    yb = yb.to(device)
+
+    logits, loss = transformer(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+    if time.time() - t0 > eval_interval:
+        losses = estimate_loss(
+            transformer,
+            n_test_batches,
+            train_data,
+            val_data,
+            batch_size=batch_size,
+            block_size=block_size,
+        )
+        pbar.set_postfix(
+            {"train_loss": f"{losses['train']:.4f}", "val_loss": f"{losses['val']:.4f}"}
+        )
+
+        eval_history.append(i)
+        for key, value in losses.items():
+            loss_history[key].append(value)
+        t0 = time.time()
+
+for key in loss_history:
+    loss_history[key] = np.asarray(loss_history[key])
+
+# %%
+with dv.FigureManager() as (_, ax):
+    ax.plot(eval_history, loss_history["train"], lw=0.5, label="train")
+    ax.plot(eval_history, loss_history["val"], lw=1.0, label="val")
+    ax.set_xlabel("batch")
+    ax.set_ylabel("loss")
+    ax.set_title("bigram model")
+
+    ax.legend(frameon=False)
+
+    best_loss = loss_history["val"].min()
+    ax.axhline(best_loss, c="gray", ls="--", lw=1.0)
+    ax.annotate(
+        f"{best_loss:.3f}",
+        (eval_history[-1], best_loss),
+        xytext=(0, -2),
+        textcoords="offset points",
+        verticalalignment="top",
+        horizontalalignment="right",
+        c="C1",
+    )
+
+# %%
+print(untokenize(transformer.generate(context=torch.tensor([[0]]), n=500)[0].tolist()))
+
+
 # %%
